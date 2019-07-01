ca88c85fa8a63c8dd9810552f300f21d1793920f
BeyondChao <gobeyondchao@gmail.com>
2019-07-01 21:47:15 +0800

    Init CriminalIntent project

---
+++ CriminalIntent/app/src/main/java/com/example/criminalintent/CrimeActivity.java
@@ -0,0 +1,14 @@
+package com.example.criminalintent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CrimeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crime);
+    }
+}
