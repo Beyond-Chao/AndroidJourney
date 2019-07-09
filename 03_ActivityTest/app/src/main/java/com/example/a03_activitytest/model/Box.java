2821dbd3b4734a23c6563f5f595f9548591eaebc
BeyondChao <gobeyondchao@gmail.com>
2019-07-09 14:57:07 +0800

    Add draw rectangle and canvas

--- 03_ActivityTest/app/src/main/java/com/example/a03_activitytest/model/Box.java
+++ 03_ActivityTest/app/src/main/java/com/example/a03_activitytest/model/Box.java
@@ -1,5 +1,7 @@
 package com.example.a03_activitytest.model;
 
+import android.graphics.PointF;
+
 /**
  * Copyright (C), 2014-2019, Yutou Technology (Hangzhou) Co, Ltd.
  * FileName: Box
@@ -7,5 +9,22 @@ package com.example.a03_activitytest.model;
  * Date: 2019-07-09
  */
 public class Box {
+    private PointF mOrigin;
+    private PointF mCurrent;
 
+    public Box(PointF origin) {
+        mOrigin = origin;
+    }
+
+    public PointF getCurrent() {
+        return mCurrent;
+    }
+
+    public void setCurrent(PointF current) {
+        mCurrent = current;
+    }
+
+    public PointF getOrigin() {
+        return mOrigin;
+    }
 }
--- 03_ActivityTest/app/src/main/java/com/example/a03_activitytest/view/BoxDrawingView.java
+++ 03_ActivityTest/app/src/main/java/com/example/a03_activitytest/view/BoxDrawingView.java
@@ -1,11 +1,19 @@
 package com.example.a03_activitytest.view;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.graphics.PointF;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+
+import com.example.a03_activitytest.model.Box;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
 
 /**
  * Copyright (C), 2014-2019, Yutou Technology (Hangzhou) Co, Ltd.
@@ -16,6 +24,11 @@ import android.view.View;
 public class BoxDrawingView extends View {
 
     private static final String TAG = "BoxDrawingView";
+    private Box mCurrentBox;
+    private List<Box> mBoxList = new ArrayList<>();
+
+    private Paint mBoxPaint;
+    private Paint mBackgroundPaint;
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
@@ -26,15 +39,23 @@ public class BoxDrawingView extends View {
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
                 action = "ACTION_DOWN";
+                mCurrentBox = new Box(current);
+                mBoxList.add(mCurrentBox);
                 break;
             case MotionEvent.ACTION_MOVE:
                 action = "ACTION_MOVE";
+                if (null != mCurrentBox) {
+                    mCurrentBox.setCurrent(current);
+                    invalidate();
+                }
                 break;
             case MotionEvent.ACTION_UP:
                 action = "ACTION_UP";
+                mCurrentBox = null;
                 break;
             case MotionEvent.ACTION_CANCEL:
                 action = "ACTION_CANEL";
+                mCurrentBox = null;
                 break;
         }
 
@@ -48,5 +69,27 @@ public class BoxDrawingView extends View {
 
     public BoxDrawingView(Context context, AttributeSet attrs) {
         super(context, attrs);
+
+        mBoxPaint = new Paint();
+        mBoxPaint.setColor(0x22ff0000);
+
+        mBackgroundPaint = new Paint();
+        mBackgroundPaint.setColor(0xfff8efe0);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        // Fill the background
+        canvas.drawPaint(mBackgroundPaint);
+
+        for (Box box : mBoxList) {
+            float left = Math.min(box.getOrigin().x, box.getCurrent().x);
+            float top = Math.min(box.getOrigin().y, box.getCurrent().y);
+
+            float right = Math.max(box.getOrigin().x, box.getCurrent().x);
+            float bottom = Math.max(box.getOrigin().y, box.getCurrent().y);
+
+            canvas.drawRect(left, top, right, bottom, mBoxPaint);
+        }
     }
 }
